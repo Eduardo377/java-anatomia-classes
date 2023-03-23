230f4030462ca8c1b9452e6c5f232940823989e3
Eduardo377 <eduardogomes377@gmail.com>
2023-03-23 15:24:48 -0300

    fix: basico de metodos

--- src/MinhaClasse.java
+++ src/MinhaClasse.java
@@ -1,6 +1,15 @@
 public class MinhaClasse {
     public static void main(String[] args) {
-        final String BR = "Brasil";
-        System.out.println(BR);
+/*        final String BR = "Brasil";
+        System.out.println(BR);*/
+        String primeiroNome = "Eduardo";
+        String segundoNome = "Gomes";
+
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+
+        System.out.println(nomeCompleto);
+    }
+    public static String nomeCompleto(String primeiroNome, String segundoNome){
+        return "Nome completo é " + primeiroNome.concat(" ").concat(segundoNome);
     }
 }
